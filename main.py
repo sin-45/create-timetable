a4d00d427da83127ca2b373f85a21a7da148f3f4
sin-45 <sin45.0403@gmail.com>
2026-02-27 15:42:38 +0900

    mainを作成 テーブルからPDFを作成するコードを追加

--- main.py
+++ main.py
@@ -14,5 +14,22 @@ def main():
     else:
         table = random_create_table.create_random_table()
 
+    day = input("必要な曜日をスペース区切りで入力してください.何も打たないと月～金となります").split()
+    if len(day) == 0:
+        day = ["月", "火", "水", "木", "金"]
+
+    new_day = [""] + day
+    new_table = [new_day[:]]
+
+    for i in range(len(table)):
+        new_table.append([f"{i+1}コマ"] + table[i])
+
+    for i in range(len(new_table)):
+        for j in range(len(new_table[i])):
+            if new_table[i][j] == None:
+                new_table[i][j] = ""
+
+    pdf_create.create_pdf(new_table, "timetable.pdf")
+
 if __name__ == "__main__":
     main()
